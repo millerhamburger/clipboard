78f45de65df35087d248118cb27868e397e6ecbe
YEYI <millerye1995@foxmail.com>
2025-11-27 17:34:15 +0800

    ``` build(rollup): 升级依赖并添加代码压缩插件
    新增 `@rollup/plugin-terser` 插件用于代码压缩，并更新相关构建配置。
    同时升级了 package.json 中的版本号及部分开发依赖。
    ```

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,15 @@
+/*
+ * @Author: YEYI millerye1995@foxmail.com
+ * @Date: 2025-11-27 17:19:31
+ * @LastEditors: YEYI millerye1995@foxmail.com
+ * @LastEditTime: 2025-11-27 17:22:57
+ * @FilePath: \clipboard\rollup.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 const typescript = require('@rollup/plugin-typescript');
 const resolve = require('@rollup/plugin-node-resolve');
 const commonjs = require('@rollup/plugin-commonjs');
+const terser = require('@rollup/plugin-terser');
 
 module.exports = {
   input: 'src/index.ts',
@@ -25,6 +34,11 @@ module.exports = {
       tsconfig: './tsconfig.json',
       declaration: true,
       declarationDir: './dist'
+    }),
+    terser({
+      compress: true,
+      mangle: true,
+      format: { comments: false }
     })
   ],
   external: []
